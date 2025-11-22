5e46f67ecd2b1dda81e5ac2e21ddb3255625f9f2
AntonellaCabB <antonellacaballerob@gmail.com>
2025-11-22 02:33:09 -0500

    conexión con mongodb (config/db.js)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,5 +17,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log('Server running on port ${PORT}');
 });
